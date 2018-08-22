7ba722968c64dd163a1e166f3c67d135f0cc0ce7
mvaudel <mvaudel@gmail.com>
2018-08-22 16:21:15 -0400

    Increased the memory given to the files.

--- scripts/makePathMatrix.sh
+++ scripts/makePathMatrix.sh
@@ -20,4 +20,4 @@ nThreads=24
 ## Script
 
 # Compute the scores on the different cohorts
-java -Xmx120G -cp $repo/bin/PathFinder-0.0.1/PathFinder-0.0.1.jar no.uib.pap.pathfinder.cmd.ExportShortestPathMatrix $nThreads
+java -d64 -XX:MaxDirectMemorySize=60g -Xmx120G -cp $repo/bin/PathFinder-0.0.1/PathFinder-0.0.1.jar no.uib.pap.pathfinder.cmd.ExportShortestPathMatrix $nThreads
